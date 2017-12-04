1d6ce0434b42fc755b8d8a6608f35c250c02b143
SCoulY <scouly@outlook.com>
2017-12-04 16:56:09 +0000

    Add files via upload

---
+++ icns_neighbour.py
@@ -0,0 +1,43 @@
+
+import icns
+import os
+import threading
+import signal
+from multiprocessing import Pool,Process
+#threads = 2
+#n = icns.Network(112)
+#nh=n.add_neighbour("131.231.114.78",111)
+#ui = icns.UI('Chat room')
+#uid = ui.getfd()
+
+def receive():
+    while 1:
+        rec_msg = n.receive()
+        ui.addline(rec_msg)
+    
+def send():
+	msg = os.read(uid,100)
+	if msg:
+		n.send(nh,msg)
+        
+def handler(signum,frame):
+    os.close(uid)
+    n.remove()
+    p.terminate()
+    
+if __name__ == '__main__':
+    threads = 2
+    n = icns.Network(112)
+    nh=n.add_neighbour("131.231.114.78",111)
+    ui = icns.UI('Chat room')
+    uid = ui.getfd()
+    rec_msg = ''
+    p = Process(target=receive,args=())
+    p.start()
+    while 1:
+#        p.join()
+        send()
+        signal.signal(signal.SIGINT,handler)
+        
+
+
