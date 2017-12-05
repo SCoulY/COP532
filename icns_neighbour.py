fddb44a43dd057eeaa5209bf48be476f7bd7c5a9
SCoulY <scouly@outlook.com>
2017-12-05 16:48:07 +0000

    Add files via upload
    reliability_layer

--- icns_neighbour.py
+++ icns_neighbour.py
@@ -1,18 +1,48 @@
 
 import icns
-import os
+import os,sys
 import threading
 import signal
+import random
 from multiprocessing import Pool,Process
 #threads = 2
 #n = icns.Network(112)
-#nh=n.add_neighbour("131.231.114.78",111)
+#nh=n.add_neighbour("131.231.114.104",111)
 #ui = icns.UI('Chat room')
 #uid = ui.getfd()
 
+class reliability:
+    def __init__(self):
+        self.ack = '0'
+        self.msgidlist = range(128)
+        self.existlist = []
+        self.seq_num = 0
+        
+    def encapsulate(self,data):
+        msgid = random.choice(list(set(self.msgidlist)-set(self.existlist)))
+        self.existlist.append(msgid)
+        header = self.ack + bin(self.msgidlist)[2:].zfill(7)
+        self.seq_num += 1
+        encapsulate_data = header + data
+        return encapsulate_data
+    
+    def decapsulate(self,data):
+        decapsulate_data = data[24:]
+        return decapsulate_data
+    
+    def send_ack(self,data,nh,n):
+        self.ack = '1'
+        ack_data = self.ack + data[1:]  
+        n.send(nh,ack_data)
+    
+    
+    
+    def set_to_default():
+        
 def receive():
     while 1:
         rec_msg = n.receive()
+#        if rec_msg 
         ui.addline(rec_msg)
     
 def send():
@@ -21,14 +51,17 @@ def send():
 		n.send(nh,msg)
         
 def handler(signum,frame):
-	os.close(uid)    
-	p.terminate()
-	n.remove()
+#    n.remove()
+    ui.stop()
+    p.terminate()
+        
     
 if __name__ == '__main__':
     threads = 2
-    n = icns.Network(112)
-    nh=n.add_neighbour("131.231.114.78",111)
+    netnumber = int(sys.argv[1])
+    neighbour = sys.argv[2].split(":")
+    n = icns.Network(netnumber)
+    nh=n.add_neighbour(neighbour[0],int(neighbour[1]))
     ui = icns.UI('Chat room')
     uid = ui.getfd()
     rec_msg = ''
@@ -36,8 +69,9 @@ if __name__ == '__main__':
     p.start()
     while 1:
 #        p.join()
-        send()
         signal.signal(signal.SIGINT,handler)
+        send()
+        
         
 
 
