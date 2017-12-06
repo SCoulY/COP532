561a8da9f72d4d5d2a37f7611a48af2d961288b4
AnYWersary <scouly@outlook.com>
2017-12-06 17:43:45 +0000

    simple_reliability

--- icns_neighbour.py
+++ icns_neighbour.py
@@ -1,6 +1,6 @@
 
 import icns
-import os,sys
+import os,sys,time
 import threading
 import signal
 import random
@@ -34,13 +34,51 @@ class reliability:
         self.ack = '1'
         ack_data = self.ack + data[1:]  
         n.send(nh,ack_data)
-    
-    
-    
+
     def set_to_default():
+
+class simple_reliability:
+    def __init__(self):
+        self.ack = '0'
+        self.packetidlist = range(128)
+        self.existlist = []
+           
+    def encapsulate(self,data):
+        packetid = random.choice(list(set(self.packetidlist)-set(self.existlist)))
+        self.existlist.append(packetid)
+        header = self.ack + bin(self.packetidlist)[2:].zfill(7)
+        encapsulate_data = header + data
+        return encapsulate_data
+    
+    def decapsulate(self,data):
+        decapsulate_data = data[8:]
+        return decapsulate_data
+
+    def send_ack(self,data,nh,n):
+        self.ack = '1'
+        ack_data = self.ack + data[1:]  
+        n.send(nh,ack_data)
         
+
+def txt2bit(input_plaintext):
+    if is_chinese_str(input_plaintext):
+        #every chinese char contains 16 bits
+        a=''.join((format(ord(x),'016b')for x in input_plaintext))
+        chinese = True
+    else:
+        #every ascii char contains 8 bits
+        a=''.join((format(ord(x),'08b')for x in input_plaintext))
+        chinese = False
+    if len(a)%792 == 0: #multiple packets and leaves 1 byte to reliability layer 
+        num_blocks = len(a)/792
+    else:#only 1 packet
+        num_blocks = len(a)/792 + 1
+    return a,num_blocks,chinese 
+
 def receive():
     while 1:
+        timeout = time.time()+0.2
+        #if a new thread is needed where to calculate timeout or it'll be stuck by n.receive()?
         rec_msg = n.receive()
 #        if rec_msg 
         ui.addline(rec_msg)
